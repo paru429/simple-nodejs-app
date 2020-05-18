440768865b9f369f0d28e8fb6c0e34b5f1b4e464
Parvathi Vallinayagam <parvathi.vallinayagam@mrcooper.com>
2020-05-18 15:35:42 +0530

    Add package.json

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
+const colors = require('colors');
 const routes = require('./routes');
 
 // createServer takes func as parameter which will be executed for every request known as request listener
 const server = http.createServer(routes);
 
 server.listen(3040);
+
+const shutdown = function() {
+  console.log(colors.yellow('WARN::************shutting down server**************'));
+  server.close();
+};
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
